1f3d201dd309c4a9fb5510af2302075eb6c26623
Isabella-brown <Isabellabrownenquiries@gmail.com>
2022-11-12 15:26:48 +0000

    changed client port to 3000

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,13 @@
 import Users from './components/users/users';
-import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [userData, setUserData] = useState([{}])
 
-  useEffect(() => {
-    fetch("/api").then(
-      res => res.json())
-      .then(
-        userData => setUserData(userData))
-  }, []);
 
 
   return (
     <div>
 
-      {/*<p>{!userData ? "Loading..." :
-        userData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      }
-    </p>*/}
     </div>
   );
 }
--- client/src/components/users/users.jsx
+++ client/src/components/users/users.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Users = () => {
+
+    const [user, setUser] = useState("")
+
+    useEffect(() => {
+
+
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => {
+                setUser(data.user)
+            })
+    }, [])
     return (
         <div>
-            <h2>users</h2>
+            <h2>{user}</h2>
         </div>
     )
 }
 
+/* const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    fetch("/api").then(
+      res => res.json())
+      .then(
+        userData => setUserData(userData.message))
+  }, []);
+
+
+  return (
+    <div>
+
+      {<p>{!userData ? "Loading..." :
+        userData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      }
+    </p>}
+    </div>
+  );
+}
+*/
 export default Users
