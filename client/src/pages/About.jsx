b06749a7beeedca0fac0559b8d00f7c39cfa4094
Isabella-brown <Isabellabrownenquiries@gmail.com>
2022-12-19 17:46:11 +0000

    styled about me page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { About__me, Profile__image, Flex__container, About__header } from "./page.styles"
+import { About__container, About__card, About__me, Profile__image, Flex__container, About__header } from "./page.styles"
+import { AiOutlineInstagram } from 'react-icons/ai'
 
 const About = () => {
     return (
         <Flex__container>
-            <About__header>About me</About__header>
-            <Profile__image src="https://media.licdn.com/dms/image/C4E03AQF-mmT0v6Zyng/profile-displayphoto-shrink_800_800/0/1657040165546?e=1677110400&v=beta&t=HVKHIMpsBEckqNfMtkGYF8bpi7GOcoXGaYN5c0w043U" />
+            <About__card>
+                <Profile__image src="https://media.licdn.com/dms/image/C4E03AQF-mmT0v6Zyng/profile-displayphoto-shrink_800_800/0/1657040165546?e=1677110400&v=beta&t=HVKHIMpsBEckqNfMtkGYF8bpi7GOcoXGaYN5c0w043U" />
+                <h3>Isabella Brown</h3>
+                <p>2D Artist | 3D Artist | Software Engineer</p>
+                <a href="https://instagram.com"><AiOutlineInstagram /></a>
+                <a href="https://www.artstation.com/isabellabrown3"><img src="https://img.icons8.com/windows/25/null/artstation.png" /></a>
+            </About__card>
+            <About__container>
+                <About__header>About me</About__header>
+                <About__me>About</About__me>
+            </About__container>
         </Flex__container>
     )
 }
--- client/src/pages/page.styles.jsx
+++ client/src/pages/page.styles.jsx
@@ -7,15 +7,31 @@ margin-top: 20px;
 margin-bottom: -60px;
 `
 const Profile__image = styled.img`
-width:20%;
+width:200px;
 border-radius:50%;
+margin-bottom:5px;
 `
 const Flex__container = styled.div`
+display:flex;
+flex-align:row;
+margin-top:20px;
 `
 const About__header = styled.h1`
-margin-left:20px;
+
 `
 const About__me = styled.p`
+display:flex;
+`
+const About__card = styled.div`
+height: 400px;
+width:40%;
+text-align:center;
+justift-content:center;
+margin-top:100px
 `
 
-export { About__me, Home__header, Profile__image, About__header, Flex__container }
+const About__container = styled.div`
+margin-left:40px;
+`
+
+export { About__container, About__card, About__me, Home__header, Profile__image, About__header, Flex__container }
